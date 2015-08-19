a2c6cd3146084536959d919aacd7b07015d21c58
franckyy <franck.calestroupat@gmail.com>
2015-08-19 17:03:46 +0200

    _

--- WikiMNV_moderation/src/fr/cap/wikimnv/moderation/service/IGSignalement.java
+++ WikiMNV_moderation/src/fr/cap/wikimnv/moderation/service/IGSignalement.java
@@ -1,9 +1,9 @@
 package fr.cap.wikimnv.moderation.service;
 
-import fr.cap.wikimnv.moderation.pojo.EtatSignalement;
 import fr.cap.wikimnv.global.service.ICRUD;
 
-public interface IGSignalement extends ICRUD{
+public interface IGSignalement extends ICRUD
+{
 	public void rejeter(Object id);
 	public void traiter(Object id);
 }
