1f69c3068056e56b1b00df127a9c80d6dda7bcca
franckyy <franck.calestroupat@gmail.com>
2015-08-19 12:53:58 +0200

    nettoyage des imports

--- WikiMNV_moderation/src/fr/cap/wikimnv/moderation/service/IModeration.java
+++ WikiMNV_moderation/src/fr/cap/wikimnv/moderation/service/IModeration.java
@@ -1,6 +1,6 @@
 package fr.cap.wikimnv.moderation.service;
 
-import fr.cap.wikimnv.service.global.IGCommentaire;
+import fr.cap.wikimnv.consultation.service.IGCommentaire;
 import fr.cap.wikimnv.service.global.IGUser;
 
 public interface IModeration {
